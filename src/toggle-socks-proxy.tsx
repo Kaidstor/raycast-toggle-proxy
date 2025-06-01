2dadfac2d50eb6640151e76659a5f7a4fce0c336
Kaidstor <kaidstor@gmail.com>
2025-06-01 20:27:39 +0500

    feat(deps): udpate

--- src/toggle-socks-proxy.tsx
+++ src/toggle-socks-proxy.tsx
@@ -8,12 +8,10 @@ export default function GenerateXrayConfig() {
   function generateXrayConfig(vlessConfig: string) {
     try {
       const url = new URL(vlessConfig);
-      // const protocol = url.protocol.replace(":", ""); // vless
       const uuid = url.username;
       const address = url.hostname;
       const port = url.port;
       const params = Object.fromEntries(url.searchParams.entries());
-      // const tag = url.hash ? decodeURIComponent(url.hash.replace("#", "")) : "";
 
 
       return {
@@ -104,33 +102,7 @@ export default function GenerateXrayConfig() {
               "type": "field",
               "ip": [
                 "geoip:ru",
-                "geoip:private",
-                "10.0.0.0/8",
-                "94.198.218.43",
-                "188.245.60.246",
-                "212.192.220.11",
-                "157.90.243.83",
-                "49.13.56.86",
-                "188.225.76.52",
-                "85.15.188.99",
-                "109.68.212.83",
-                "84.201.152.45",
-                "158.160.161.15",
-                "151.248.125.33",
-                "185.20.226.173",
-                "176.99.12.14",
-                "185.182.111.128",
-                "5.63.155.77",
-                "213.189.201.82",
-                "194.67.118.219",
-                "194.67.118.219",
-                "95.163.235.88",
-                "134.0.116.239",
-                "31.31.203.138",
-                "95.163.236.131",
-                "51.250.40.221",
-                "51.250.40.221",
-                "80.90.182.105"
+                "geoip:private"
               ],
               "outboundTag": "direct"
             }]
@@ -145,7 +117,7 @@ export default function GenerateXrayConfig() {
     try {
       const config = generateXrayConfig(vlessConfig);
       setXrayConfig(JSON.stringify(config, null, 2));
-      showToast(Toast.Style.Success, "Конфигурация успешно сгенерирована!");
+      showToast(Toast.Style.Success, "Конфигурация успешно сгенерирована и сохранена в папке расширения");
     } catch (error) {
       showToast(Toast.Style.Failure, (error as Error).message);
     }
